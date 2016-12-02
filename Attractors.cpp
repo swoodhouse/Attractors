d36c67f77725f1669dc01ccddb509ace2490ca83
Steven Woodhouse <t-stwood@microsoft.com>
2016-12-02 11:16:47 +0000

    Pass by const reference

--- Attractors.cpp
+++ Attractors.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-BDD representState(Cudd manager, std::vector<bool> values) {
+BDD representState(const Cudd& manager, const std::vector<bool>& values) {
     BDD bdd = manager.bddOne();
     int i = 0;
     for (auto it = std::begin(values); it != std::end(values); ++it) {
@@ -14,7 +14,7 @@ BDD representState(Cudd manager, std::vector<bool> values) {
     return bdd;
 }
 
-BDD representCnf(Cudd manager, std::vector<int> cnf) {
+BDD representCnf(const Cudd& manager, const std::vector<int>& cnf) {
     BDD bdd = manager.bddZero();
     for (auto it = std::begin(cnf); it != std::end(cnf); ++it) {
         BDD var = manager.bddVar(abs(*it));
@@ -26,7 +26,7 @@ BDD representCnf(Cudd manager, std::vector<int> cnf) {
     return bdd;
 }
 
-BDD representUpdateFunction(Cudd manager, std::vector<std::vector<int>> cnfs) {
+BDD representUpdateFunction(const Cudd& manager, const std::vector<std::vector<int>>& cnfs) {
     BDD bdd = manager.bddOne();
     for (auto it = std::begin(cnfs); it != std::end(cnfs); ++it) {
         bdd = bdd * representCnf(manager, *it);
@@ -34,11 +34,15 @@ BDD representUpdateFunction(Cudd manager, std::vector<std::vector<int>> cnfs) {
     return bdd;
 }
 
-BDD logicalEquivalence(BDD a, BDD b) {
+BDD implication(const BDD& a, const BDD& b) {
+    return (!a) + b;
+}
+
+BDD logicalEquivalence(const BDD& a, const BDD& b) {
     return (a * b) + ((!a) * (!b));
 }
 
-BDD representSyncTransitionRelation(Cudd manager, std::vector<std::vector<std::vector<int>>> network) {
+BDD representSyncTransitionRelation(const Cudd& manager, const std::vector<std::vector<std::vector<int>>>& network) {
     BDD bdd = manager.bddOne();
     int i = 0;
     for (auto it = std::begin(network); it != std::end(network); ++it) {
@@ -51,7 +55,7 @@ BDD representSyncTransitionRelation(Cudd manager, std::vector<std::vector<std::v
     return bdd;
 }
 
-BDD otherVarsDoNotChange(Cudd manager, int i, int numVars) {
+BDD otherVarsDoNotChange(const Cudd& manager, int i, int numVars) {
     BDD bdd = manager.bddOne();
     for (int j = 0; j < numVars; j++) {
         if (j != i) {
@@ -63,7 +67,7 @@ BDD otherVarsDoNotChange(Cudd manager, int i, int numVars) {
     return bdd;
 }
 
-BDD representAsyncTransitionRelation(Cudd manager, std::vector<std::vector<std::vector<int>>> network) {
+BDD representAsyncTransitionRelation(const Cudd& manager, const std::vector<std::vector<std::vector<int>>>& network) {
     BDD fixpoint = manager.bddOne();
     for (int i = 0; i < network.size(); i++) {
         BDD v = manager.bddVar(i);
@@ -87,7 +91,7 @@ BDD representAsyncTransitionRelation(Cudd manager, std::vector<std::vector<std::
     return bdd;
 }
 
-BDD renameRemovingPrimes(BDD bdd, int numVars) {
+BDD renameRemovingPrimes(const BDD& bdd, int numVars) {
     int *permute = new int[numVars * 2];
     for (int i = 0; i < numVars; i++) {
         permute[i] = i;
@@ -98,11 +102,11 @@ BDD renameRemovingPrimes(BDD bdd, int numVars) {
     return r;
 }
 
-BDD nonPrimeVariables(Cudd manager, int numVars) {
+BDD nonPrimeVariables(const Cudd& manager, int numVars) {
     return representState(manager, std::vector<bool>(numVars, true));
 }
 
-BDD primeVariables(Cudd manager, int numVars) {
+BDD primeVariables(const Cudd& manager, int numVars) {
     BDD bdd = manager.bddOne();
     for (int i = numVars; i < numVars * 2; i++) {
         BDD var = manager.bddVar(i);
@@ -111,14 +115,14 @@ BDD primeVariables(Cudd manager, int numVars) {
     return bdd;
 }
 
-BDD immediateSuccessorStates(Cudd manager, BDD transitionBdd, BDD valuesBdd, int numVars) {
+BDD immediateSuccessorStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numVars) {
     BDD bdd = transitionBdd * valuesBdd;
     bdd = bdd.ExistAbstract(nonPrimeVariables(manager, numVars));
     bdd = renameRemovingPrimes(bdd, numVars);
     return bdd;
 }
 
-BDD forwardReachableStates(Cudd manager, BDD transitionBdd, BDD valuesBdd, int numVars) {
+BDD forwardReachableStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numVars) {
     BDD reachable = manager.bddZero();
     BDD frontier = valuesBdd;
 
@@ -129,7 +133,7 @@ BDD forwardReachableStates(Cudd manager, BDD transitionBdd, BDD valuesBdd, int n
     return reachable;
 }
 
-BDD renameAddingPrimes(BDD bdd, int numVars) {
+BDD renameAddingPrimes(const BDD& bdd, int numVars) {
     int *permute = new int[numVars * 2];
     for (int i = 0; i < numVars; i++) {
         permute[i] = i + numVars;
@@ -141,14 +145,14 @@ BDD renameAddingPrimes(BDD bdd, int numVars) {
     return r;
 }
 
-BDD immediatePredecessorStates(Cudd manager, BDD transitionBdd, BDD valuesBdd, int numVars) {
-    valuesBdd = renameAddingPrimes(valuesBdd, numVars);
-    BDD bdd = transitionBdd * valuesBdd;
+BDD immediatePredecessorStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numVars) {
+    BDD bdd = renameAddingPrimes(valuesBdd, numVars);
+    bdd = transitionBdd * valuesBdd;
     bdd = bdd.ExistAbstract(primeVariables(manager, numVars));
     return bdd;
 }
 
-BDD backwardReachableStates(Cudd manager, BDD transitionBdd, BDD valuesBdd, int numVars) {
+BDD backwardReachableStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numVars) {
     BDD reachable = manager.bddZero();
     BDD frontier = valuesBdd;
 
@@ -159,17 +163,17 @@ BDD backwardReachableStates(Cudd manager, BDD transitionBdd, BDD valuesBdd, int 
     return reachable;
 }
 
-BDD randomState(Cudd manager, BDD S, int numVars) {
+BDD randomState(const Cudd& manager, const BDD& S, int numVars) {
     char *out = new char[numVars * 2];
     S.PickOneCube(out);
 
     std::vector<bool> values;
     for (int i = 0; i < numVars; i++) {
-        if (out[i] == 0) {
+        if (out[i] == 0) { // careful with = vs == in c++
             values.push_back(false);
         }
         else {
-            values.push_back(true);
+            values.push_back(true); // ideally, on 2 you would choose randomly between 0 and 1. may not matter
         }
     }
 
@@ -177,7 +181,7 @@ BDD randomState(Cudd manager, BDD S, int numVars) {
     return representState(manager, values);
 }
 
-std::vector<BDD> attractors(Cudd manager, BDD transitionBdd, int numVars) {
+std::vector<BDD> attractors(const Cudd&  manager, const BDD& transitionBdd, int numVars) {
     std::vector<BDD> attractors = std::vector<BDD>();
     BDD S = manager.bddOne();
     while (S != manager.bddZero()) {
@@ -196,19 +200,19 @@ std::vector<BDD> attractors(Cudd manager, BDD transitionBdd, int numVars) {
 
 int main() {
     //Cebpa, Pu.1, Gata2, Gata1, Fog1, EKLF, Fli1, Scl, cJun, EgrNab, Gfi1
-    std::vector<bool> cmpInitial { true, true, true, false, false, false, false, false, false, false, false };
-    std::vector<std::vector<int>> cebpa { { 0 },{ -7 },{ -3, -4 } };
-    std::vector<std::vector<int>> pu1 { { 0, 1 },{ -2 },{ -3 } };
-    std::vector<std::vector<int>> gata2 { { 3 },{ -1 },{ -3, -4 } };
-    std::vector<std::vector<int>> gata1 { { 2, 3, 6 },{ -1 } };
-    std::vector<std::vector<int>> fog1 { { 3 } };
-    std::vector<std::vector<int>> eklf { { 3 },{ -6 } };
-    std::vector<std::vector<int>> fli1 { { 3 },{ -5 } };
-    std::vector<std::vector<int>> scl { { 3 },{ -1 } };
-    std::vector<std::vector<int>> cJun { { 1 },{ -10 } };
-    std::vector<std::vector<int>> egrNab { { 1 },{ 8 },{ -10 } };
-    std::vector<std::vector<int>> gfi1 { { 0 },{ -9 } };
-    std::vector<std::vector<std::vector<int>>> cmp { cebpa, pu1, gata2, gata1, fog1, eklf, fli1, scl, cJun, egrNab, gfi1 };
+    std::vector<bool> cmpInitial{ true, true, true, false, false, false, false, false, false, false, false };
+    std::vector<std::vector<int>> cebpa{ { 0 },{ -7 },{ -3, -4 } };
+    std::vector<std::vector<int>> pu1{ { 0, 1 },{ -2 },{ -3 } };
+    std::vector<std::vector<int>> gata2{ { 3 },{ -1 },{ -3, -4 } };
+    std::vector<std::vector<int>> gata1{ { 2, 3, 6 },{ -1 } };
+    std::vector<std::vector<int>> fog1{ { 3 } };
+    std::vector<std::vector<int>> eklf{ { 3 },{ -6 } };
+    std::vector<std::vector<int>> fli1{ { 3 },{ -5 } };
+    std::vector<std::vector<int>> scl{ { 3 },{ -1 } };
+    std::vector<std::vector<int>> cJun{ { 1 },{ -10 } };
+    std::vector<std::vector<int>> egrNab{ { 1 },{ 8 },{ -10 } };
+    std::vector<std::vector<int>> gfi1{ { 0 },{ -9 } };
+    std::vector<std::vector<std::vector<int>>> cmp{ cebpa, pu1, gata2, gata1, fog1, eklf, fli1, scl, cJun, egrNab, gfi1 };
 
     Cudd manager(0, 0);
     BDD initialStateBdd = representState(manager, cmpInitial);
