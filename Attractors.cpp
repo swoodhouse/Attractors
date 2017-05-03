996b71a91fd6f23a0369e3dd2f9693b7fbcddc64
Steven Woodhouse <steven.j.woodhouse@gmail.com>
2017-05-03 10:09:26 +0100

    Functionally complete, but in need of refactoring

--- Attractors.cpp
+++ Attractors.cpp
@@ -1,17 +1,17 @@
 #include "stdafx.h"
 
 BDD representState(const Cudd& manager, const std::vector<bool>& values) {
-    BDD bdd = manager.bddOne();
-    int i = 0;
-    for (auto it = std::begin(values); it != std::end(values); ++it) {
-        BDD var = manager.bddVar(i);
-        if (!*it) {
-            var = !var;
-        }
-        bdd = var * bdd;
-        i++;
-    }
-    return bdd;
+	BDD bdd = manager.bddOne();
+	int i = 0;
+	for (auto it = std::begin(values); it != std::end(values); ++it) {
+		BDD var = manager.bddVar(i);
+		if (!*it) {
+			var = !var;
+		}
+		bdd = var * bdd;
+		i++;
+	}
+	return bdd;
 }
 
 BDD representClause(const Cudd& manager, const std::vector<int>& clause) {
@@ -35,136 +35,133 @@ BDD representUpdateFunction(const Cudd& manager, const std::vector<std::vector<i
 }
 
 BDD implication(const BDD& a, const BDD& b) {
-    return !a + b;
+	return !a + b;
 }
 
 BDD logicalEquivalence(const BDD& a, const BDD& b) {
-    return implication(a, b) * implication(b, a);
+	return implication(a, b) * implication(b, a);
 }
 
 BDD representSyncTransitionRelation(const Cudd& manager, const std::vector<std::vector<std::vector<int>>>& network) {
-    BDD bdd = manager.bddOne();
-    int i = 0;
-    for (auto it = std::begin(network); it != std::end(network); ++it) {
-        BDD f = representUpdateFunction(manager, *it);
-        BDD vPrime = manager.bddVar(network.size() + i);
-        BDD transition = logicalEquivalence(vPrime, f);
-        bdd = transition * bdd;
-        i++;
-    }
-    return bdd;
+	BDD bdd = manager.bddOne();
+	int i = 0;
+	for (auto it = std::begin(network); it != std::end(network); ++it) {
+		BDD f = representUpdateFunction(manager, *it);
+		BDD vPrime = manager.bddVar(network.size() + i);
+		BDD transition = logicalEquivalence(vPrime, f);
+		bdd = transition * bdd;
+		i++;
+	}
+	return bdd;
 }
 
 BDD otherVarsDoNotChange(const Cudd& manager, int i, int numVars) {
-    BDD bdd = manager.bddOne();
-    for (int j = 0; j < numVars; j++) {
-        if (j != i) {
-            BDD v = manager.bddVar(j);
-            BDD vPrime = manager.bddVar(numVars + j);
-            bdd = bdd * logicalEquivalence(v, vPrime);
-        }
-    }
-    return bdd;
+	BDD bdd = manager.bddOne();
+	for (int j = 0; j < numVars; j++) {
+		if (j != i) {
+			BDD v = manager.bddVar(j);
+			BDD vPrime = manager.bddVar(numVars + j);
+			bdd = bdd * logicalEquivalence(v, vPrime);
+		}
+	}
+	return bdd;
 }
 
 BDD representAsyncTransitionRelation(const Cudd& manager, const std::vector<std::vector<std::vector<int>>>& network) {
-    BDD fixpoint = manager.bddOne();
-    for (size_t i = 0; i < network.size(); i++) {
-        BDD v = manager.bddVar(i);
-        BDD vPrime = manager.bddVar(network.size() + i);
-        fixpoint = fixpoint * logicalEquivalence(v, vPrime);
-    }
+	BDD fixpoint = manager.bddOne();
+	for (size_t i = 0; i < network.size(); i++) {
+		BDD v = manager.bddVar(i);
+		BDD vPrime = manager.bddVar(network.size() + i);
+		fixpoint = fixpoint * logicalEquivalence(v, vPrime);
+	}
 
-    BDD bdd = manager.bddZero();
-    int i = 0;
-    for (auto it = std::begin(network); it != std::end(network); ++it) {
-        BDD v = manager.bddVar(i);
-        BDD vPrime = manager.bddVar(network.size() + i);
-        BDD vChanges = !logicalEquivalence(v, vPrime);
-        BDD f = representUpdateFunction(manager, *it);
-        BDD update = logicalEquivalence(vPrime, f) * otherVarsDoNotChange(manager, i, network.size());
-        BDD transition = update * (fixpoint + vChanges);
-        bdd = transition + bdd;
-        i++;
-    }
+	BDD bdd = manager.bddZero();
+	int i = 0;
+	for (auto it = std::begin(network); it != std::end(network); ++it) {
+		BDD v = manager.bddVar(i);
+		BDD vPrime = manager.bddVar(network.size() + i);
+		BDD vChanges = !logicalEquivalence(v, vPrime);
+		BDD f = representUpdateFunction(manager, *it);
+		BDD update = logicalEquivalence(vPrime, f) * otherVarsDoNotChange(manager, i, network.size());
+		BDD transition = update * (fixpoint + vChanges);
+		bdd = transition + bdd;
+		i++;
+	}
 
-    return bdd;
+	return bdd;
 }
 
 BDD renameRemovingPrimes(const BDD& bdd, int numUnprimedBDDVars) {
-    int *permute = new int[numUnprimedBDDVars * 2];
-    for (int i = 0; i < numUnprimedBDDVars; i++) {
-        permute[i] = i;
-        permute[i + numUnprimedBDDVars] = i;
-    }
-    BDD r = bdd.Permute(permute);
-    delete[] permute;
-    return r;
+	int *permute = new int[numUnprimedBDDVars * 2];
+	for (int i = 0; i < numUnprimedBDDVars; i++) {
+		permute[i] = i;
+		permute[i + numUnprimedBDDVars] = i;
+	}
+	BDD r = bdd.Permute(permute);
+	delete[] permute;
+	return r;
 }
 
 BDD nonPrimeVariables(const Cudd& manager, int numUnprimedBDDVars) {
-    return representState(manager, std::vector<bool>(numUnprimedBDDVars, true));
+	return representState(manager, std::vector<bool>(numUnprimedBDDVars, true));
 }
 
 BDD primeVariables(const Cudd& manager, int numUnprimedBDDVars) {
-    BDD bdd = manager.bddOne();
-    for (int i = numUnprimedBDDVars; i < numUnprimedBDDVars * 2; i++) {
-        BDD var = manager.bddVar(i);
-        bdd = var * bdd;
-    }
-    return bdd;
+	BDD bdd = manager.bddOne();
+	for (int i = numUnprimedBDDVars; i < numUnprimedBDDVars * 2; i++) {
+		BDD var = manager.bddVar(i);
+		bdd = var * bdd;
+	}
+	return bdd;
 }
 
 BDD immediateSuccessorStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numUnprimedBDDVars) {
-    BDD bdd = transitionBdd * valuesBdd;
-    bdd = bdd.ExistAbstract(nonPrimeVariables(manager, numUnprimedBDDVars));
-    bdd = renameRemovingPrimes(bdd, numUnprimedBDDVars);
-
-    return bdd;
+	BDD bdd = transitionBdd * valuesBdd;
+	bdd = bdd.ExistAbstract(nonPrimeVariables(manager, numUnprimedBDDVars));
+	return renameRemovingPrimes(bdd, numUnprimedBDDVars);
 }
 
 BDD forwardReachableStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numUnprimedBDDVars) {
-    BDD reachable = manager.bddZero();
-    BDD frontier = valuesBdd;
+	BDD reachable = manager.bddZero();
+	BDD frontier = valuesBdd;
 
-    while (frontier != manager.bddZero()) {
-        frontier = immediateSuccessorStates(manager, transitionBdd, frontier, numUnprimedBDDVars) * !reachable;
-        reachable = reachable + frontier;
-    }
-    return reachable;
+	while (frontier != manager.bddZero()) {
+		frontier = immediateSuccessorStates(manager, transitionBdd, frontier, numUnprimedBDDVars) * !reachable;
+		reachable = reachable + frontier;
+	}
+	return reachable;
 }
 
 BDD renameAddingPrimes(const BDD& bdd, int numUnprimedBDDVars) {
-    int *permute = new int[numUnprimedBDDVars * 2];
-    for (int i = 0; i < numUnprimedBDDVars; i++) {
-        permute[i] = i + numUnprimedBDDVars;
-        permute[i + numUnprimedBDDVars] = i + numUnprimedBDDVars;
-    }
+	int *permute = new int[numUnprimedBDDVars * 2];
+	for (int i = 0; i < numUnprimedBDDVars; i++) {
+		permute[i] = i + numUnprimedBDDVars;
+		permute[i + numUnprimedBDDVars] = i + numUnprimedBDDVars;
+	}
 
-    BDD r = bdd.Permute(permute);
-    delete[] permute;
-    return r;
+	BDD r = bdd.Permute(permute);
+	delete[] permute;
+	return r;
 }
 
 BDD immediatePredecessorStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numUnprimedBDDVars) {
-    BDD bdd = renameAddingPrimes(valuesBdd, numUnprimedBDDVars);
-    bdd = transitionBdd * bdd;
-    bdd = bdd.ExistAbstract(primeVariables(manager, numUnprimedBDDVars));
-    return bdd;
+	BDD bdd = renameAddingPrimes(valuesBdd, numUnprimedBDDVars);
+	bdd = transitionBdd * bdd;
+	return bdd.ExistAbstract(primeVariables(manager, numUnprimedBDDVars));
 }
 
 BDD backwardReachableStates(const Cudd& manager, const BDD& transitionBdd, const BDD& valuesBdd, int numUnprimedBDDVars) {
-    BDD reachable = manager.bddZero();
-    BDD frontier = valuesBdd;
+	BDD reachable = manager.bddZero();
+	BDD frontier = valuesBdd;
 
-    while (frontier != manager.bddZero()) {
-        frontier = immediatePredecessorStates(manager, transitionBdd, frontier, numUnprimedBDDVars) * !reachable;
-        reachable = reachable + frontier;
-    }
-    return reachable;
+	while (frontier != manager.bddZero()) {
+		frontier = immediatePredecessorStates(manager, transitionBdd, frontier, numUnprimedBDDVars) * !reachable;
+		reachable = reachable + frontier;
+	}
+	return reachable;
 }
 
-BDD randomState(const Cudd& manager, BDD S, int numVars) {
+BDD randomState(const Cudd& manager, const BDD& S, int numVars) {
 	char *out = new char[numVars * 2];
 	S.PickOneCube(out);
 	std::vector<bool> values;
@@ -186,6 +183,12 @@ std::list<BDD> attractorsBN(const Cudd&  manager, const BDD& transitionBdd, int 
 
 	while (S != manager.bddZero()) {
 		BDD s = randomState(manager, S, numVars);
+
+		for (int i = 0; i < 20; i++) {
+			BDD sP = immediateSuccessorStates(manager, transitionBdd, s, numVars);
+			s = randomState(manager, sP, numVars);
+		}
+
 		BDD fr = forwardReachableStates(manager, transitionBdd, s, numVars);
 		BDD br = backwardReachableStates(manager, transitionBdd, s, numVars);
 
@@ -250,7 +253,7 @@ BDD representPrimedVarQN(const Cudd& manager, int var, int val, const std::vecto
 
 BDD representStateQN(const Cudd& manager, const std::vector<int>& vars, const std::vector<int>& values, const std::vector<int>& ranges) {
 	BDD bdd = manager.bddOne();
-	for (/*std::vector<int>::size_type*/ size_t i = 0; i < vars.size(); i++) {
+	for (size_t i = 0; i < vars.size(); i++) {
 		int var = vars[i];
 		int val = values[i];
 		bdd = bdd * representUnprimedVarQN(manager, var, val, ranges);
@@ -258,22 +261,21 @@ BDD representStateQN(const Cudd& manager, const std::vector<int>& vars, const st
 	return bdd;
 }
 
-BDD removeInvalidBitCombinations(const Cudd& manager, const BDD& S, const std::vector<int>& ranges) {
-	BDD bdd = S;
+BDD removeInvalidBitCombinations(const Cudd& manager, BDD S, const std::vector<int>& ranges) {
 	for (int var = 0; var < ranges.size(); var++) {
 		if (ranges[var] > 0) {
 			int b = bits(ranges[var]);
 			int theoreticalMax = (1 << b) - 1;
 
 			for (int val = ranges[var] + 1; val <= theoreticalMax; val++) {
-				bdd = bdd * !representUnprimedVarQN(manager, var, val, ranges);
+				S = S * !representUnprimedVarQN(manager, var, val, ranges);
 			}
 		}
 	}
-	return bdd;
+	return S;
 }
 
-BDD representSyncQNTransitionRelation(const Cudd& manager, const std::vector<int>& ranges, const std::vector<std::vector<int>>& inputVars,
+BDD representSyncQNTransitionRelation(/*const*/ Cudd& manager, const std::vector<int>& ranges, const std::vector<std::vector<int>>& inputVars,
 	const std::vector<std::vector<std::vector<int>>>& inputValues, const std::vector<std::vector<int>>& outputValues) {
 	BDD bdd = manager.bddOne();
 	int v = 0;
@@ -299,20 +301,20 @@ BDD representSyncQNTransitionRelation(const Cudd& manager, const std::vector<int
 	return bdd;
 }
 
-std::list<BDD> attractorsQN(Cudd manager, const BDD& transitionBdd, const std::vector<int>& ranges) {
+std::list<BDD> attractorsQN(Cudd manager, const BDD& transitionBdd, const std::vector<int>& ranges, const BDD& statesToRemove) {
 	auto lambda = [](int a, int b) { return a + bits(b); };
 	int numUnprimedBDDVars = std::accumulate(ranges.begin(), ranges.end(), 0, lambda);
 	std::list<BDD> attractors;
 	BDD S = manager.bddOne();
 	S = removeInvalidBitCombinations(manager, S, ranges);
-
-	manager.ReduceHeap(CUDD_REORDER_SIFT, 0);
+	S = S * !(statesToRemove);
+	manager.ReduceHeap(CUDD_REORDER_SIFT, 0); 
 
 	while (S != manager.bddZero()) {
 		BDD s = randomState(manager, S, numUnprimedBDDVars);
 
 		for (int i = 0; i < 20; i++) {
-			BDD sP = forwardReachableStates(manager, transitionBdd, s, numUnprimedBDDVars);
+			BDD sP = immediateSuccessorStates(manager, transitionBdd, s, numUnprimedBDDVars);
 			s = randomState(manager, sP, numUnprimedBDDVars);
 		}
 
@@ -398,4 +400,190 @@ BDD representAsyncQNTransitionRelation(/*const*/ Cudd& manager, const std::vecto
 	}
 
 	return bdd;
+}
+
+bool isAsyncLoop(const Cudd& manager, const BDD& S, const BDD& syncTransitionBdd, const std::vector<int>& ranges) {
+	auto lambda = [](int a, int b) { return a + bits(b); };
+	int numUnprimedBDDVars = std::accumulate(ranges.begin(), ranges.end(), 0, lambda);
+	BDD reached = manager.bddZero();
+	BDD s = randomState(manager, S, numUnprimedBDDVars);
+
+	while (s != manager.bddZero()) {
+		BDD sP = immediateSuccessorStates(manager, syncTransitionBdd, s, numUnprimedBDDVars);
+		char *sCube = new char[numUnprimedBDDVars * 2];
+		s.PickOneCube(sCube);
+		char *sPCube = new char[numUnprimedBDDVars * 2];
+		sP.PickOneCube(sPCube);
+
+		int nVarDiff = 0;
+		int i = 0;
+		for (int range : ranges) {
+			int b = bits(range);
+			for (int j = i; j < i + b; j++) {
+				if (sCube[j] != sPCube[j]) {
+					nVarDiff++;
+					break;
+				}
+			}
+			if (nVarDiff >= 2) return false;
+			i += b;
+		}
+		s = sP * !reached;
+		reached = reached + s;
+	}
+	return true;
+}
+
+std::list<BDD> combinedAlgorithm(Cudd manager, const BDD& syncTransitionBdd, const BDD& asyncTransitionBdd, const std::vector<int>& ranges) {
+	auto lambda = [](int a, int b) { return a + bits(b); };
+	int numUnprimedBDDVars = std::accumulate(ranges.begin(), ranges.end(), 0, lambda);
+	std::list<BDD> asyncAttractors;
+	std::list<BDD> syncAttractors = attractorsQN(manager, syncTransitionBdd, ranges, manager.bddZero());
+	BDD syncAsyncAttractors = manager.bddZero();
+	for (BDD a : syncAttractors) {
+		if (isAsyncLoop(manager, a, syncTransitionBdd, ranges)) {
+			syncAsyncAttractors = syncAsyncAttractors + a;
+			asyncAttractors.push_back(a);
+		}
+	}
+	BDD br = backwardReachableStates(manager, asyncTransitionBdd, syncAsyncAttractors, numUnprimedBDDVars);
+	asyncAttractors.splice(asyncAttractors.end(), attractorsQN(manager, asyncTransitionBdd, ranges, syncAsyncAttractors + br));
+	return asyncAttractors;
+}
+
+std::string printRange(const std::list<int>& values) {
+	auto lambda = [](std::string a, int b) { return a + "; " + std::to_string(b); };
+	return "[" + std::accumulate(std::next(values.begin()), values.end(), std::to_string(values.front()), lambda) + "]";
+}
+
+std::string fromBinary(const std::string& bits, int offset) {
+	int i = 0;
+	auto lambda = [&i](int a) { return a + std::pow(2.0f, i); };
+	std::list<int> values{ offset };
+
+	for (auto it = std::rbegin(bits); it < std::rend(bits); ++it) {
+		if (*it == '-') {
+			std::list<int> copy(values);
+			std::transform(copy.begin(), copy.end(), copy.begin(), lambda);
+			values.splice(values.end(), copy);
+		}
+		else if (*it == '1') {
+			std::transform(values.begin(), values.end(), values.begin(), lambda);
+		}
+		i++;
+	}
+	return values.size() > 1 ? printRange(values) : std::to_string(values.front());
+}
+
+std::string prettyPrint(const Cudd& manager, const BDD& attractor, const std::vector<int>& ranges, const std::vector<int>& offsets) {
+	FILE *old = manager.ReadStdout();
+	FILE *fp = fopen("temp.txt", "w");
+	manager.SetStdout(fp);
+	attractor.PrintMinterm();
+	manager.SetStdout(old);
+	fclose(fp);
+
+	std::string out;
+	std::ifstream infile("temp.txt");
+	std::string line;
+	auto lambda = [](std::string a, std::string b) { return a + ", " + b; };
+	while (std::getline(infile, line)) {
+		std::list<std::string> output;
+		int i = 0;
+		for (int v = 1; v < ranges.size(); v++) {
+			if (ranges[v] == 0) {
+				output.push_back(std::to_string(offsets[v]));
+			}
+			else {
+				int b = bits(ranges[v]);
+				output.push_back(fromBinary(line.substr(i, b), offsets[v]));
+				i += b;
+			}
+		}
+		out += std::accumulate(std::next(output.begin()), output.end(), output.front(), lambda) + "\n";
+	}
+
+
+	return out;
+}
+
+extern "C" __declspec(dllexport) int attractors(int numVars, int ranges[], int offsets[], int numInputs[], int inputVars[], int numUpdates[], int inputValues[], int outputValues[], int mode) {
+	std::vector<int> rangesV(ranges, ranges + numVars);
+	std::vector<int> offsetsV(offsets, offsets + numVars);
+	std::vector<std::vector<int>> inputVarsV;
+	std::vector<std::vector<int>> outputValuesV;
+	std::vector<std::vector<std::vector<int>>> inputValuesV;
+
+	int k = 0;
+	for (int i = 0; i < numVars; i++) {
+		std::vector<int> in;
+		for (int j = 0; j < numInputs[i]; j++) {
+			in.push_back(inputVars[k]);
+			k++;
+		}
+		inputVarsV.push_back(in);
+	}
+
+	k = 0;
+	for (int i = 0; i < numVars; i++) {
+		std::vector<int> out;
+		for (int j = 0; j < numUpdates[i]; j++) {
+			out.push_back(outputValues[k]);
+			k++;
+		}
+		outputValuesV.push_back(out);
+	}
+
+	k = 0;
+	for (int i = 0; i < numVars; i++) {
+		std::vector<std::vector<int>> in;
+		for (int j = 0; j < numUpdates[i]; j++) {
+			std::vector<int> v;
+			for (int l = 0; l < numInputs[i]; l++) {
+				v.push_back(inputValues[k]);
+				k++;
+			}
+			in.push_back(v);
+		}
+		inputValuesV.push_back(in);
+	}
+
+	Cudd manager(0, 0);
+
+	if (mode == 0) {
+		std::cout << "Building synchronous transition relation..." << std::endl;
+		BDD transitionBdd = representSyncQNTransitionRelation(manager, rangesV, inputVarsV, inputValuesV, outputValuesV);
+
+		std::cout << "Finding attractors..." << std::endl;
+		std::list<BDD> atts = attractorsQN(manager, transitionBdd, rangesV, manager.bddZero());
+		int i = 0;
+		for (const auto& attractor : atts) {
+			std::cout << "Attractor " << i << ":" << std::endl;
+			attractor.PrintMinterm();
+			std::cout << prettyPrint(manager, attractor, rangesV, offsetsV) << std::endl;
+			i++;
+		}
+		return i;
+	}
+	else {
+		std::cout << "Building synchronous transition relation..." << std::endl;
+		BDD syncTransitionBdd = representSyncQNTransitionRelation(manager, rangesV, inputVarsV, inputValuesV, outputValuesV);
+
+		std::cout << "Building asynchronous transition relation..." << std::endl;
+		BDD asyncTransitionBdd = representAsyncQNTransitionRelation(manager, rangesV, inputVarsV, inputValuesV, outputValuesV);
+
+		std::cout << "Finding attractors with combined algorithm..." << std::endl;
+		std::list<BDD> atts = combinedAlgorithm(manager, syncTransitionBdd, asyncTransitionBdd, rangesV);
+		int i = 0;
+
+		std::cout << "Pretty printing..." << std::endl;
+
+		for (const auto& attractor : atts) {
+			std::cout << "Attractor " << i << ":" << std::endl;
+			attractor.PrintMinterm();
+			std::cout << prettyPrint(manager, attractor, rangesV, offsetsV) << std::endl;
+			i++;
+		}
+		return i;
+	}
 }
